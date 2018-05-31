fa7a3dd749eaf81dbd0c3a47087e2d0bb29dbcad
lijian <lijiancode@gmail.com>
2018-05-31 16:48:01 +0800

    'v1.0'

--- sqldb.py
+++ sqldb.py
@@ -1,35 +1,42 @@
 import wget
 import sqlite3
+import os
 
+# status=0 初始化默认值;status=1 下载失败，status=2 进行中,status=3 已完成
 conn = sqlite3.connect('file.db', check_same_thread=False)
 c = conn.cursor()
-
 
 def insert(url):
     hasfile = c.execute("select * from file where url='{fileUrl}'".format(fileUrl=url))
     if hasfile.fetchone():
         pass
     else:
-        c.execute("insert into file(url) values('{fileUrl}')".format(fileUrl=url))
+        c.execute("insert into file(url,status) values('{fileUrl}',0)".format(fileUrl=url))
         conn.commit()
-
 
 def download(id):
     if id > 0:
-        filedata = c.execute("select * from file where id='{id}'".format(id=id))
+        filedata = c.execute("select * from file where id='{id}' and status <= 1".format(id=id))
     else:
-        filedata = c.execute("select * from file where status is null")
+        filedata = c.execute("select * from file where status = 0")
     for item in filedata:
         try:
-            filename = wget.download(item[1], 'static/video')
-            c.execute("update file set status =1 where id='{id}'".format(id=item[0]))
-            c.execute("update file set name ='{name}' where id='{id}'".format(name=filename, id=item[0]))
+            c.execute("update file set status =2 where id='{id}'".format(id=item[0]))
             conn.commit()
+            filename = wget.download(item[1], 'static/video')
+            extension = ['py','pyc','php','js','sh']
+            if filename.split('.')[-1] in extension:
+                c.execute("update file set status =4 where id='{id}'".format(id=item[0]))
+                conn.commit()
+                os.remove(os.getcwd()+"/static/video/"+filename)
+            else:
+                c.execute("update file set status =3 where id='{id}'".format(id=item[0]))
+                c.execute("update file set name ='{name}' where id='{id}'".format(name=filename, id=item[0]))
+                conn.commit()
         except BaseException:
-            c.execute("update file set status =0 where id='{id}'".format(id=item[0]))
+            c.execute("update file set status =1 where id='{id}'".format(id=item[0]))
             conn.commit()
     return 'success'
-
 
 def getData():
     result = c.execute("select * from file order by id desc").fetchall()
